3e10c31135ddf058caf2cac2d34cd06d1cf636cf
2015tb145-acare <alban.care@gmail.com>
2021-12-14 14:44:15 +0100

    install & configure react-router-dom + add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="display-1">Boostrap !</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div className="container p-5">
+      <h1 className="display-3 text-light text-center">
+        Auth React with firebase
+      </h1>
+    </div>
+  );
+}
